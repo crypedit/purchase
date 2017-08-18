9fecd29e9db11e530f687ce7b579705bab980d2a
Yan Qian <qianyan@thoughtworks.com>
2017-08-18 21:41:02 +0800

    add requirement of deployment to ropsten network

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ Purchase
 - [ ] 仅卖家在合约状态为新建状态时才能终止合约
 - [ ] 仅买家确认购买
 - [ ] 仅买家确认收货，合约才算完成
+
+## 部署到 testnet (ropsten)
+参考`truffle.js`中的注释的内容，部署该智能合约到`ropsten`即`http://45.76.197.72/:8545`上。
+```
+$ cp your_keystore_file ./keystore
+$ printf "your_passphase" > ./pass
+$ truffle deploy --network ropsten
+```
+**注意**
+
+你的账户中部署时必须有点`ether`，那么如何获取testnet上的`ehter`呢？
+
--- truffle.js
+++ truffle.js
@@ -1,9 +1,20 @@
+// Following is used for Production Release, uncomment when deployment
+// var WalletProvider = require("truffle-wallet-provider");
+// var keystore = require('fs').readFileSync('./keystore').toString();
+// var pass = require('fs').readFileSync('./pass').toString();
+// var wallet = require('ethereumjs-wallet').fromV3(keystore, pass);
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    // ropsten: {
+    //     network_id: 1,
+    //     provider: new WalletProvider(wallet, "http://45.76.197.72/:8545"),
+    // }
   }
 };
+
