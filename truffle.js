ed2da020b45be7a0a2992ecdaef003af3052f077
Yan Qian <qianyan@thoughtworks.com>
2018-04-22 22:03:15 +0800

    refactor truffle.js with hd wallet

--- truffle.js
+++ truffle.js
@@ -1,20 +1,18 @@
-// Following is used for Production Release, uncomment when deployment
-// var WalletProvider = require("truffle-wallet-provider");
-// var keystore = require('fs').readFileSync('./keystore').toString();
-// var pass = require('fs').readFileSync('./pass').toString();
-// var wallet = require('ethereumjs-wallet').fromV3(keystore, pass);
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+      port: 9545,
+      network_id: "*"
     },
-    // ropsten: {
-    //     network_id: 1,
-    //     provider: new WalletProvider(wallet, "http://45.76.197.72:8545"),
-    // }
+    rinkeby: {
+      network_id: 0,
+      provider: new HDWalletProvider(
+        process.env.METAMASK_MNEMONIC,
+        "https://ropsten.infura.io/LtCp6vYseQHwZxs0i94S"
+      )
+    }
   }
 };
-
